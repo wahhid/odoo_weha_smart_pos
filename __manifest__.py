594dd71f220ec683a8e9cecb7b2f1f3ef6814e0e
weha <wahhid@weha-id.com>
2021-03-11 12:05:23 +0700

    First Coding

---
+++ __manifest__.py
@@ -0,0 +1,19 @@
+# License AGPL-3.0 or later (http://www.gnu.org/licenses/agpl).
+
+{
+    'name': 'WEHA - Smart Pos',
+    'summary': """Smart Point of Sale""",
+    'version': '14.0.0.1.0',
+    'license': 'AGPL-3',
+    'category': 'Point of sale',
+    'author': 'WEHA',
+    'website': 'https://www.weha-id.com',
+    'depends': ['base'],
+    'data': [
+        'security/weha_smart_pos_templates.xml',
+        'views/weha_smart_post_templates.xml'
+    ],
+    'application': True,
+    'installable': True,
+    "auto_install": False,
+}
---
+++ controllers/main.py
@@ -0,0 +1,15 @@
+# -*- coding: utf-8 -*-
+
+from odoo import http
+from odoo.http import content_disposition, request
+import logging
+
+
+_logger = logging.getLogger(__name__)
+
+
+class WehaSmartPosController(http.Controller):
+    @http.route(['/weha_pos/pos',], type='http', auth="user", csrf=False)
+    def pos(self,**args):
+        return http.request.render('weha_smart_pos.pos_screen',{})
+    
