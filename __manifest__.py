3f31f2ffcebc8284515e9e43be6162efb666a7b9
weha <wahhid@weha-id.com>
2021-03-11 12:27:26 +0700

    Add Module Category

--- __manifest__.py
+++ __manifest__.py
@@ -10,6 +10,7 @@
     'website': 'https://www.weha-id.com',
     'depends': ['base'],
     'data': [
+        'data/weha_smart_pos_data.xml',
         'security/weha_smart_pos_security.xml',
         'views/weha_smart_post_templates.xml'
     ],
