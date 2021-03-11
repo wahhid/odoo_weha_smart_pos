226f54efea07f03664afd09317c067fcfa4f7a20
weha <wahhid@weha-id.com>
2021-03-11 12:30:45 +0700

    Fix Main Controller

--- controllers/main.py
+++ controllers/main.py
@@ -11,5 +11,5 @@ _logger = logging.getLogger(__name__)
 class WehaSmartPosController(http.Controller):
     @http.route(['/weha_pos/pos',], type='http', auth="user", csrf=False)
     def pos(self,**args):
-        return http.request.render('weha_smart_pos.pos_screen',{})
+        return http.request.render('odoo_weha_smart_pos.pos_screen',{})
     
