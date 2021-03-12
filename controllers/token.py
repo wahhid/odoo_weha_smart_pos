ffd0ca9fec4798dfd84c19e353079c3be4e8e13b
weha <wahhid@weha-id.com>
2021-03-12 21:03:08 +0700

    Uppdate API

--- controllers/token.py
+++ controllers/token.py
@@ -5,13 +5,13 @@ import logging
 import werkzeug.wrappers
 
 from odoo import http
-from odoo.addons.weha_voucher_mgmt.common import invalid_response, valid_response
+from odoo.addons.odoo_weha_smart_pos.common import invalid_response, valid_response
 from odoo.http import request
 from odoo.exceptions import AccessError, AccessDenied
 
 _logger = logging.getLogger(__name__)
 
-expires_in = "weha_voucher_mgmt.access_token_expires_in"
+expires_in = "odoo_weha_smart_pos.access_token_expires_in"
 
 
 class AccessToken(http.Controller):
