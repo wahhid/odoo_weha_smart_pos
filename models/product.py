e8525b0430be9584c991f774780df047ad5662ef
weha <wahhid@weha-id.com>
2021-04-26 10:47:44 +0700

    Fix Pos Product Category

--- models/product.py
+++ models/product.py
@@ -2,8 +2,7 @@ from odoo import models, fields
 
 
 class ProductTemplate(models.Model):
-    _name = 'product.template'
+    _inherit = 'product.template'
 
     is_avaiable_on_pos = fields.Boolean('Available on Pos', default=False)
-    pos_product_category_id = fields.Many2one('smart.pos.product.category', "Pos Product Category")
-    
+    smart_pos_product_category_id = fields.Many2one('smart.pos.product.category', "Pos Product Category")
