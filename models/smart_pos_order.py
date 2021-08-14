3f8cd4e41a8140e23f1bcb7f44dfe5013ed05545
weha <wahhid@weha-id.com>
2021-08-14 15:02:04 +0700

    update transaction upload

--- models/smart_pos_order.py
+++ models/smart_pos_order.py
@@ -17,7 +17,7 @@ class SmartPosOrder(models.Model):
         vals = {
             'partner_id': self.partner_id.id if self.partner_id else False,
             'user_id': False,
-            'picking_type_id': 6,
+            'picking_type_id': picking_type_id.id,
             'location_id': picking_type_id.default_location_src_id.id,
             'location_dest_id': picking_type_id.default_location_dest_id.id
         }
@@ -29,6 +29,17 @@ class SmartPosOrder(models.Model):
         self.create_stock_move()
 
 
+            # 'name': first_line.name,
+            # 'product_uom': first_line.product_id.uom_id.id,
+            # 'picking_id': self.id,
+            # 'picking_type_id': self.picking_type_id.id,
+            # 'product_id': first_line.product_id.id,
+            # 'product_uom_qty': abs(sum(order_lines.mapped('qty'))),
+            # 'state': 'draft',
+            # 'location_id': self.location_id.id,
+            # 'location_dest_id': self.location_dest_id.id,
+            # 'company_id': self.company_id.id,
+
     def create_stock_move(self):
         for smart_pos_order_line_id in self.smart_pos_order_line_ids:
             move = self.env['stock.move'].create({
@@ -39,6 +50,7 @@ class SmartPosOrder(models.Model):
                 'product_id': smart_pos_order_line_id.product_id.id,
                 'product_uom': smart_pos_order_line_id.product_id.uom_id.id,
                 'product_uom_qty': smart_pos_order_line_id.qty,
+                'state': 'draft',
             })
             move._action_confirm()
             move._action_assign()
